cdaab272396ff4e8f4844d9ac101c682f93fd80b
anggiramadhan2910 <anggiramadhan2910@gmail.com>
2025-02-21 06:51:56 +0700

    komit kedua

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app') @section('content')
+<div class ="container">
+<h1>Home</h1>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias quas, fugiat, aperiam ipsum, architecto voluptatum nemo nihil natus iure recusandae vel eveniet enim minus distinctio reiciendis maiores perferendis porro perspiciatis.</p>
+</div>
+@endsection
